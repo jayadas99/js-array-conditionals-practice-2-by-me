b63d53e48768283cd3b3a4a9f0a0af83c3b6395a
jayadas99 <dasshiuly54@gmail.com>
2023-01-27 19:12:51 +0600

    my commoit

--- practice-3-triangle.js
+++ practice-3-triangle.js
@@ -1,10 +1,19 @@
-var line1 = 8;
-var line2 = 9;
+var line1 = 9;
+var line2 = 8;
 var line3 = 9;
 
-if(line1 == line2 || line1 == line3 || line2 == line3){
+// if(line1 == line2 || line1 == line3 || line2 == line3 ){
+//     console.log("Isosceles");
+// }
+if(line1==line2 && line2==line3){
+    console.log("Equilateral Triangle");
+}
+
+else if(((line1==line2) && (line1 != line3 && line2 !=line3)) || ((line2==line3) && (line2 != line1 && line3 !=line1))|| ((line1==line3) && (line1 != line2 && line3 !=line2))){
     console.log("Isosceles");
 }
+
+
 // else if(line2 == line1 || line2 == line3){
 //     console.log("Isosceles");
 // }
